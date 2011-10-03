db33c307ac044d82d3a59fb91b60f428c18e025f
Shinya Amemiya <shinya@ged.lan>
2011-10-04 00:00:12 +0900

    upload

---
+++ ObjCToCppTemplate/main.mm
@@ -0,0 +1,37 @@
+
+#include <vector>
+#include <boost/scoped_ptr.hpp>
+
+using namespace std;
+using namespace boost;
+
+template <class T>
+class ToCpp
+{
+public:
+    typedef T ElementType;
+
+    ToCpp(T* const t) : t_(t) {}
+    T* get() const { return t_; }
+
+private:
+    T* t_;
+};
+
+@interface MyClass : NSObject
+@end
+
+@implementation MyClass
+@end
+
+typedef ToCpp<MyClass> RappedMyClass;
+
+int main()
+{
+    RappedMyClass mc([[MyClass alloc] init]);
+
+    vector<RappedMyClass> v;
+    v.push_back(mc);
+
+    return 0;
+}
